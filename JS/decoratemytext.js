68872a55c144891f120e4a9b802380f4d90ae888
Lulit25 <lulitm5489@gmail.com>
2021-09-05 23:25:37 -0400

    corrections

--- JS/decoratemytext.js
+++ JS/decoratemytext.js
@@ -1,90 +1,82 @@
-function decorate(){
+document.addEventListener("DOMContentLoaded",function(event){
+    var txt = document.getElementById("txtarea");
+    var check =  document.getElementById("bling");
+    var btn= document.getElementById("btn");
+
+    btn.onclick = function(){
+        setInterval(bigger, 500);
+    }
     
-
-    var txt = document.getElementById("text")
-    var style = window.getComputedStyle(txt, null).getPropertyValue('font-size');    
-     alert(style);    
-    var size = parseFloat(style); 
-    document.getElementById("text").style.fontSize =(size + 2) + 'px';
-
-}
-
-const checkbox = document.getElementById('check');
-checkbox.addEventListener('change', (event) =>{
-
-    if ( event.target.checked ) {
-        alert( "checked");
-        document.getElementById("text").style.fontWeight ="bold";
-        document.getElementById("text").style.color ="green";
-        document.getElementById("text").style.textDecoration ="underline";
-        document.body.style.backgroundImage = "url('images/background.png')";
-
+   function bigger(){
+    var computedFontSize =parseInt( window.getComputedStyle(txt,null).getPropertyValue("font-size"));
+    var size = computedFontSize + 2 ;
+    txt.style.fontSize = size +"px";
 
         
-    } else {
-        // Returns false if not checked
-        document.getElementById("text").style.fontWeight ="";
-        document.getElementById("text").style.color ="black";
-        document.getElementById("text").style.textDecoration ="none";
-        document.body.style.backgroundImage = "url('')";
-
     }
-
-
-   
-});
-
-btn2=document.getElementById("btn2"); 
-var txt = document.getElementById("text");
-btn2.onclick =function(){
-    var output =" ";
-    const str = txt.value.split(" ");
-    for(let i=0; i<str.length;i++){
   
-        var start = str[i].charAt(0);
-        let temp = " ";
-        switch(start){
-            case 'a' :
-            case 'e':
-            case 'i' :
-            case 'o':
-            case 'u' :
-            case 'A' :
-            case 'E':
-            case 'I' :
-            case 'O':
-            case 'U' :
-                temp = str[i] +"ay ";
-                output = output + temp;
-                temp =" ";
-            break;
-            default:
-                temp = str[i].substring(1);
-                temp = temp + start + "ay ";
-                output = output + temp;
-                temp =" ";
-            break;
-
-        }
+   check.onchange = function(){
        
+       if(check.checked ==true){
+        txt.style.fontWeight = "bold";
+        txt.style.color ="green";
+        txt.style.textDecoration="underline";
+        document.body.style.backgroundImage ="url(images/background.png)";
+       } else {
+        txt.style.fontWeight = "normal";
+        txt.style.textDecoration="none";
+        txt.style.color ="black";
+        document.body.style.background ="none";
+       }
+
     }
-    txt.value = output;
-}
-btn1=document.getElementById("btn1"); 
+    btn2.onclick = function(){
+        var output =" ";
+        const str = txt.value.split(" ");
+        for(let i=0; i<str.length;i++){
+      
+            var start = str[i].charAt(0);
+            let temp = " ";
+            switch(start){
+                case 'a' :
+                case 'e':
+                case 'i' :
+                case 'o':
+                case 'u' :
+                case 'A' :
+                case 'E':
+                case 'I' :
+                case 'O':
+                case 'U' :
+                    temp = str[i] +"ay ";
+                    output = output + temp;
+                    temp =" ";
+                break;
+                default:
+                    temp = str[i].substring(1);
+                    temp = temp + start + "ay ";
+                    output = output + temp;
+                    temp =" ";
+                break;
 
-btn1.onclick = function(){
-    var txt2 = document.getElementById("text");
-
-    var output =" ";
-    const str = txt2.value.split(" ");
-    let temp =" ";
-    for(let i=0; i<str.length;i++){
-        if(str[i].length>=5){
-            temp ="Malkovitch";
-        }else {
-            temp = str[i];
+            }
+           
         }
-       output = output+ temp +" ";
+        txt.value = output;
     }
-    txt2.value = output;
-}
+    btn1.onclick = function(){
+    
+        var out =" ";
+        const str2 = txt.value.split(" ");
+        let temp =" ";
+        for(let i=0; i<str2.length;i++){
+            if(str2[i].length>=5){
+                temp ="Malkovitch";
+            }else {
+                temp = str2[i];
+            }
+           out = out+ temp +" ";
+        }
+        txt.value = out;
+    } 
+})
